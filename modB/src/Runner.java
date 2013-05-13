e4327748f82943088e42cbfdd1faa03bb8c42e91
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2013-05-13 13:23:57 +0400

    gg

--- modB/src/Runner.java
+++ modB/src/Runner.java
@@ -2,12 +2,6 @@ import a.b.c.B1;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Created with IntelliJ IDEA.
- * User: user
- * Date: 5/8/13
- * Time: 2:34 PM
- */
 public class Runner {
     public static void main(String[] args) {
         AbstractApplicationContext parent = new ClassPathXmlApplicationContext("classpath:/A1.xml");
